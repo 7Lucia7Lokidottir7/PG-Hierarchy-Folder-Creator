79bda9085ddfa866d4616b554d1cbfe837b59bd1
DESKTOP-88LO54M\Bordi <Bordiyan Georgii>
2025-07-22 01:15:57 +0400

    Update.

---
+++ Hierarchy Folder Creator/Assets/PG Hierarchy Folder Creator/Runtime/SeparatorNameChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+namespace PG.HierarchyFolder
+{
+    public class SeparatorNameChanger : MonoBehaviour
+    {
+        private const string preName = "-----------";
+        private const string afterName = " --------------------------";
+        [SerializeField] private new string name = "Name";
+        public string fullName => preName + name + afterName;
+        private void OnValidate()
+        {
+            base.name = fullName;
+        }
+        [ContextMenu("Remove Component")]
+        void DestroySeparator()
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
