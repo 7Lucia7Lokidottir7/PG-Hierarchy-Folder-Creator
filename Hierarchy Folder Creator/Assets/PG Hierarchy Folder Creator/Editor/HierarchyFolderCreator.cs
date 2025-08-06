4a79f74e88eaf92b9aa99d6dd32d15c6f2a23dcb
DESKTOP-88LO54M\Bordi <Bordiyan Georgii>
2025-08-06 12:08:53 +0400

    Update sort.

--- Hierarchy Folder Creator/Assets/PG Hierarchy Folder Creator/Editor/HierarchyFolderCreator.cs
+++ Hierarchy Folder Creator/Assets/PG Hierarchy Folder Creator/Editor/HierarchyFolderCreator.cs
@@ -5,7 +5,7 @@ namespace PG.HierarchyFolder
 {
     public class HierarchyFolderCreator : MonoBehaviour
     {
-        [MenuItem("GameObject/Folder",priority =0)]
+        [MenuItem("GameObject/PG/Sort/Folder",priority =0)]
         static void Create()
         {
             GameObject folder = Resources.Load("PG Hierarchy Folder Creator/Folder") as GameObject;
@@ -13,7 +13,7 @@ namespace PG.HierarchyFolder
             folderInstance.name = folder.name;
             Selection.activeObject = folderInstance;
         }
-        [MenuItem("GameObject/Separator", priority = 0)]
+        [MenuItem("GameObject/PG/Sort/Separator", priority = 0)]
         static void CreateSeparator()
         {
             GameObject separator = Resources.Load("PG Hierarchy Folder Creator/Separator") as GameObject;
